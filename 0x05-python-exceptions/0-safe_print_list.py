5329e551c2e039b4099d8de1e8971bd24ccf48a7
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-08-28 15:38:45 +0100

    fix 1.1.1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         if my_list == []:
             return 0
         for i in range(x):
-            print(my_list[i], end=" ")
+            print(my_list[i], end="")
         print()
         return i + 1
     except IndexError:
