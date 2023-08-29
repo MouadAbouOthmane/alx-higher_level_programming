aeb12394c79c07661d4690ce08100ea647398bea
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-08-30 00:44:26 +0100

    Create 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Square module."""
+
+class Square:
+    """Define a square."""
+	
+    def __init__(self, size = 0):
+        """initialization method
+
+        Args:
+            size: size of square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """property of size of square
+        
+        Raises:
+            TypeError: size must be an integer.
+            ValueError: if size is less than 0.
+        """
+        return self.__size
+        
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Area of square
+
+        Returns:
+             Current square area.
+        """
+        return self.__size ** 2
