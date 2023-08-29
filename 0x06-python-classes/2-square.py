b8222f808366cce807d785c2f6c4d5ce592a342e
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-08-29 23:23:43 +0000

    2. Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Square module."""
+
+class Square:
+    """Define a square."""
+	
+    def __init__(self, size = 0):
+        """initialization method
+
+        Args:
+            size: size of square.
+        Raises:
+            TypeError: size must be an integer.
+            ValueError: if size is less than 0.
+        """
+        if type(size) == int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
