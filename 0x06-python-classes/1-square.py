466f6732dff570b65fd621dc0594fc225af418cd
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-08-30 00:01:37 +0100

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square module."""
+
+class Square:
+    """Define a square."""
+	
+	def __init__(self, size)
+	"""Init method
+
+	Args:
+ 		size: size of square.
+ 	"""
+		self.__size = size 
