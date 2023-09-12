16610fcf7ab4d41015f02e1e61c8865ad3f08abe
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-12 23:53:21 +0000

    10. Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""10. Student to JSON with filter TASK"""
+
+
+class Student:
+    """
+    Class Student that defines a student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        dic = {}
+        for key in vars(self):
+            if key in attrs:
+                dic[key] = self.__dict__[key]
+        return dic
