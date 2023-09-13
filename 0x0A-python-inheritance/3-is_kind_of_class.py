8716a006ad0c0775b60bcc5e9ec7f76d0d6b8e8a
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-13 13:58:48 +0000

    3. Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""3. Same class or inherit from TASK"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Function that returns True if the object
+    is an instance of a class that inherited; otherwise False
+    """
+    return isinstance(obj, a_class):
