4831d8fb625bec0c37915fcf181f087276c32745
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-13 14:20:59 +0000

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""6. Improve Geometry TASK"""
+
+
+class BaseGeometry:
+    """
+    Geometry class
+    """
+
+    def area(self):
+        """
+        Raise an exception
+
+        Raises:
+            Exception: area() is not implement
+        """
+        raise Exception('area() is not implemented')
