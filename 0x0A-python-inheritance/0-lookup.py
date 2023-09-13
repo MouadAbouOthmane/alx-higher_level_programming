5c727f9a5c7299d613a787245704c50af83a2626
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-13 13:13:12 +0000

    0. Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""0. Lookup TASK"""
+
+
+def lookup(obj):
+    """
+    Function that returns the list of available
+    attributes and methods of an object
+    """
+    return dir(obj)
