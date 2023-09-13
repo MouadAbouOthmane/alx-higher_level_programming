0181a7de8b9ad925412464eaed0696d74ce551cb
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-13 14:27:15 +0000

    7. Integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""7. Integer validator TASK"""
+
+
+class BaseGeometry:
+    """
+    Geometry class
+    """
+
+    def area(self):
+        """
+        Raise an exception
+
+        Raises:
+            Exception: area() is not implement
+        """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """method that validates value
+
+        Args:
+            name: a string
+            value: must be an ineger
+        Raises:
+            TypeError: <name> must be an integer
+            ValueError: <name> must be greater than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
