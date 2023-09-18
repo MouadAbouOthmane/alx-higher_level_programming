b0a02e8a8514af2abcdc0dac7487beea69bd8aea
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-18 21:05:31 +0000

    5. Display #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,3 +77,8 @@ class Rectangle(Base):
     def area(self):
         """returns the area value of the Rectangle"""
         return self.__width * self.__height
+
+    def display(self):
+        """prints in stdout the Rectangle with the character #"""
+        for _ in range(self.__height):
+            print('#' * self.__width)
