fae316bf90b33c27e61a00bc7090da63c730be60
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-18 22:19:04 +0000

    10. And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""10. And now, the Square! TASK """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class Square inherits from Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        instatiation
+
+        Args:
+            size: rectangle size
+            x: rectangle axis x
+            y: rectangle axis y
+        """
+        super().__init__(size, size, x, y, id)
+
+
+    @property
+    def size(self):
+        """size property"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+
+    def __str__(self):
+        """__str__ method """
+        return "[Square] (" \
+            + f"{self.id}) {self.__x}/{self.__y} - "\
+            + f"{self.width}"
+
+    def update(self, *args, **kwargs):
+        """update the argument of instance"""
+        if len(args) > 0:
+            try:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except IndexError:
+                pass
+        else:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                elif key == 'size':
+                    self.size = value
+                elif key == 'x':
+                    self.x = value
+                elif key == 'y':
+                    self.y = value
