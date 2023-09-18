9c36cc20a3f1735202104d6bb1a4df8a1c745aca
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-18 18:19:15 +0000

    2. First Rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""2. First Rectangle TASK"""
+Base = __import__('base').Base
+
+
+class Rectangle(Base):
+    """Class Rectangle inherits from Base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """constructor"""
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """width property"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """height property"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """x property"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """y property"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
