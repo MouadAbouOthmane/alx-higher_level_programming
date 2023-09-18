3a4ccf0f66a884d4b4c27c318aef6d7165a3c7ad
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-18 21:18:00 +0000

    6. __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """2. First Rectangle TASK """
-from models.base import Base
+# from models.base import Base
+Base = __import__('base').Base
 
 
 class Rectangle(Base):
@@ -82,3 +83,9 @@ class Rectangle(Base):
         """prints in stdout the Rectangle with the character #"""
         for _ in range(self.__height):
             print('#' * self.__width)
+
+    def __str__(self):
+        """__str__ method """
+        return "[Rectangle] (" \
+            + f"{self.id}) <{self.__x}>/<{self.__y}> - "\
+            + f"<{self.__width}>/<{self.__height}>"
