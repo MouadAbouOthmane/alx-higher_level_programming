e99c670eb1c8ac34d5dce2e4e12b62953e7af931
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-18 19:14:27 +0000

    ..

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """2. First Rectangle TASK """
-Base = __import__('base').Base
+from models.base import Base
 
 
 class Rectangle(Base):
-    """Class Rectangle inherits from Base
-    """
+    """Class Rectangle inherits from Base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
