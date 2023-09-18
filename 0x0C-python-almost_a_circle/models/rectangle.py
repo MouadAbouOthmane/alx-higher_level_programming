1a90e4f045fd2d45b54a4043d6ded3834418159f
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-09-18 22:32:27 +0000

    13. Rectangle instance to dictionary representation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """2. First Rectangle TASK """
-from models.base import Base
+# from models.base import Base
+Base = __import__('base').Base
+
 
 
 class Rectangle(Base):
@@ -113,3 +115,7 @@ class Rectangle(Base):
                     self.__x = value
                 elif key == 'y':
                     self.__y = value
+
+    def to_dictionary(self):
+        return {'x': self.x, 'y': self.y, 
+                'id': self.id, 'height': self.__height, 'width': self.__width}
