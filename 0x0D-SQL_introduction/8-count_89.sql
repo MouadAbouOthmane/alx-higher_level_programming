acb669c8f594d295973c4f3135694f69a5d8d470
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-10-18 00:43:15 +0100

    add COUNT to return the number of records

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- 8. Count 89
 -- Displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0
-SELECT * FROM first_table WHERE id = 89;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
