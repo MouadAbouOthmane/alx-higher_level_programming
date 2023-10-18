d201fb178668d6645127cddebf527d8c1da5722f
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-10-18 01:44:55 +0100

    sort the number descending

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- 15. Number by score
 -- Lists the number of records with the same score in the table second_table of the database hbtn_0c_0
-SELECT score, COUNT(name) as number FROM second_table GROUP BY score;
+SELECT score, COUNT(name) as number FROM second_table GROUP BY score ORDER BY number DESC;
