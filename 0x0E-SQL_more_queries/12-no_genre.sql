7efce81d8d0e855a6d9412d52d6134127d96ffe6
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-10-19 17:57:55 +0100

    style sql 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,6 +3,6 @@
 
 SELECT S.title AS title, SG.genre_id AS genre_id
   FROM tv_show_genres AS SG
-  RIGHT JOIN tv_shows AS S ON SG.show_id = S.id
-  WHERE SG.show_id IS NULL
-  ORDER BY S.title, SG.genre_id;
+ RIGHT JOIN tv_shows AS S ON SG.show_id = S.id
+ WHERE SG.show_id IS NULL
+ ORDER BY S.title, SG.genre_id;
