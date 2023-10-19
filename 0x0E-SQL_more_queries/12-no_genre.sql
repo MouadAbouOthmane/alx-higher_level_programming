2d19b471657b8de49a82a09a86f96482fe099f75
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-10-19 17:55:53 +0100

    12. No genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- 12. No genre
+--  lists all shows contained in hbtn_0d_tvshows without a genre linked.
+
+SELECT S.title AS title, SG.genre_id AS genre_id
+  FROM tv_show_genres AS SG
+  RIGHT JOIN tv_shows AS S ON SG.show_id = S.id
+  WHERE SG.show_id IS NULL
+  ORDER BY S.title, SG.genre_id;
