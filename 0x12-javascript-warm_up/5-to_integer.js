cf68866cc7b72aafed9bff76bb9bd1ab5d002d03
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-11-13 21:58:12 +0100

    semistandard problem

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('process');
 
-let num = parseInt(argv[2]);
+const num = parseInt(argv[2]);
 
 console.log(Number.isInteger(num) ? 'My number: ' + num : 'Not a number');
