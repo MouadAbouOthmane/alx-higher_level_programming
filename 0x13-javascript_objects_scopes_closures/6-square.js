e1da61f4533358767c67a6a7d0d9b0a2e0aee33b
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-11-14 23:12:15 +0100

    6. Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (s) {
+    super(s, s);
+  }
+
+  charPrint (c) {
+    let l;
+
+    if (c) l = c;
+    else l = 'X';
+
+    for (let i = 0, k = ''; i < this.width; k = '', i++) {
+      for (let j = 0; j < this.height; k += l, j++);
+      console.log(k);
+    }
+  }
+}
+
+module.exports = Square;
