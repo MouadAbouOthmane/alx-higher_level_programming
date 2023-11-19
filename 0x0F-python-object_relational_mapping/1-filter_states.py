735583392b16f2df85a0bf9dee046733c1893fcb
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-11-19 18:53:57 +0100

    1. Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""0. Get all states"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+
+    cur = db.cursor()
+
+    sql = "select * from states where name like 'N%';"
+
+    cur.execute(sql)
+
+    res = cur.fetchall()
+
+    for x in res:
+        print(x)
+
+    cur.close()
+    db.close()
