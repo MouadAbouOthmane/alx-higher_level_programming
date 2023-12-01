36db9e82ab1887f256ab2b4a814440e24ca9c6ff
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-12-01 22:00:13 +0100

    8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""8. Search API"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = 'https://httpbin.org/post'
+    arg = sys.argv[1]
+    r = requests.post(url, data={'q': arg}).text
+    try:
+        json = r.json()
+        if json == '{}':
+            print('No result')
+        else:
+            print(json)
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
+    # if r.status_code == requests.codes.ok:
+    #     print(r.text)
+    # else:
+    #     print('Error code:', r.status_code)
