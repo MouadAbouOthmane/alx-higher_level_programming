386e4b17f157d815960f44ce39462192c8facfc8
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-12-01 21:49:07 +0100

    7. Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""7. Error code #1"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    r = requests.get(url)
+    if r.status_code == requests.codes.ok:
+        print(r.text)
+    else:
+        print('Error code:', r.status_code)
