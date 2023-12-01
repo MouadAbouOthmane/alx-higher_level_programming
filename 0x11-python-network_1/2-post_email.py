1d270253bead7fff17fff870142e36553de91c9c
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-12-01 19:58:49 +0100

    2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""2. POST an email #0"""
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    value = {'email', email}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
