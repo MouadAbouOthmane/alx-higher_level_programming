1d3e38954aebde8034a57d2e01ded96718de1b6d
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2023-12-01 21:17:29 +0100

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     import sys
 
     url = sys.argv[1]
-    with urllib.request.urlopen(url)\
-            as response:
+    with urllib.request.urlopen(url) as response:
         header = response.headers._headers
         print(next(i[1] for i in header if i[0] == 'X-Request-Id'))
