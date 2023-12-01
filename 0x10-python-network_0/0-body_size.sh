0f88f6facb0f50e4297a47d2333b36f6bf5b21d9
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-12-01 17:02:42 +0100

    --silent ot -s

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays the size of the body of the response
-curl $1 --silent -w '%{stderr}%{size_download}\n' 1> /dev/null
+curl $1 -s -w '%{stderr}%{size_download}\n' 1> /dev/null
