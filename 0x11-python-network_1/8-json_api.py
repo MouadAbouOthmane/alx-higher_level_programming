096dfa7815b20b752b5d2dd189264f9b72a127ce
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-12-01 22:16:31 +0100

    test case 3

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,17 +5,17 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    url = 'http://0.0.0.0:5000/search_user'
+    url = 'https://httpbin.org/post'
     arg = sys.argv[1] if len(sys.argv) == 2 else ""
-    r = requests.post(url, data={'q': arg}).text
-    try:
-        json = r.json()
-        if json == '{}':
-            print('No result')
-        else:
-            print(json)
-    except Exception:
-        print("Not a valid JSON")
+    r = requests.post(url, data={'q': arg})
+    # try:
+    g = r.json()
+    if g == '{}':
+        print('No result')
+    else:
+        print(g)
+    # except Exception:
+    #     print("Not a valid JSON")
     # if r.status_code == requests.codes.ok:
     #     print(r.text)
     # else:
