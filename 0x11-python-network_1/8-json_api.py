391544aa85b42567ee56f5cb046377d559d94ab7
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-12-01 22:21:09 +0100

    last one

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,15 +8,11 @@ if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
     arg = sys.argv[1] if len(sys.argv) == 2 else ""
     r = requests.post(url, data={'q': arg})
-    # try:
-    g = r.json()
-    if g == '{}':
-        print('No result')
-    else:
-        print(g.text)
-    # except Exception:
-    #     print("Not a valid JSON")
-    # if r.status_code == requests.codes.ok:
-    #     print(r.text)
-    # else:
-    #     print('Error code:', r.status_code)
+    try:
+        g = r.json()
+        if g == {}:
+            print('No result')
+        else:
+            print(g)
+    except Exception:
+        print("Not a valid JSON")
