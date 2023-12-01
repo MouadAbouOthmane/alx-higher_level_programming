0bb747ed02961b4227687dc6b46e204c6f23db23
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2023-12-01 19:09:28 +0100

    1. Response header value #0

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""1. Response header value #0"""
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+    url = sys.argv[1]
+    with urllib.request.urlopen(url)\
+            as response:
+        header = response.headers._headers
+        print(next(i[1] for i in header if i[0] == 'X-Request-Id'))
