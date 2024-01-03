ac7123336e93e8983d2a753aa3f6442e52071d83
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2024-01-03 23:23:59 +0100

    0. Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const process = require('process');
+const fs = require('fs');
+
+const filename = process.argv[2];
+
+fs.readFile(filename, 'utf-8', (err, data) => {
+  if (err) console.log(err);
+  else {
+    console.log(data);
+  }
+});
